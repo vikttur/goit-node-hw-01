c35f4281dfbc0ef96dd02fb615a78783dd0267bc
Viktor Biruk <vikttur.job@gmail.com>
2023-07-29 17:57:19 +0300

    creates a console application

--- contacts.js
+++ contacts.js
@@ -1,7 +1,8 @@
 const { nanoid } = require('nanoid');
 const fs = require('fs').promises;
 const path = require('path');
-const contactsPath = path.join(__dirname,'contacts.json');
+
+const contactsPath = path.join(__dirname,'db','contacts.json');
 
 const listContacts = async() => {
 	const contacts = await fs.readFile(contactsPath);
@@ -14,28 +15,15 @@ const getContactById = async (id) => {
 	return contacts.find(contact => contact.id === contactId ) || null;
 }
 
-const addContact = async (name, email, phone) => {
+const addContact = async( params ) => {
 	const contacts = await listContacts();
 	const newContact = {
 		id: nanoid(),
-		name,
-		email,
-		phone,
+		...params,
 	};
 	contacts.push(newContact);
 	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 	return newContact;
-}
-
-const updateContact = async (id, params) => {
-	const contactId = String(id);
-	const contacts = await listContacts(); 
-	const index = contacts.findIndex(item => item.id === contactId); 
-	if (index === -1) return null;
-
-	contacts[index] = { contactId, ...params }; 
-	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-	return index;
 }
 
 const removeContact = async (id) => {
@@ -49,4 +37,16 @@ const removeContact = async (id) => {
 	return  result;
 }
 
-module.exports = { listContacts, getContactById, addContact, updateContact, removeContact }
+// additionally
+const updateContact = async (id, params) => {
+	const contactId = String(id);
+	const contacts = await listContacts(); 
+	const index = contacts.findIndex(item => item.id === contactId); 
+	if (index === -1) return null;
+
+	contacts[index] = { id, ...params }; 
+	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+	return contacts[index];
+}
+
+module.exports = { listContacts, getContactById, addContact, removeContact, updateContact }
--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const readline = require('readline');
-const yargs = require('yargs');
-const { hideBin } = require('yargs/yelpers');
-const { Command } = require('commander');
+const { program } = require('commander');
 const contacts = require('./contacts');
-
-const program = new Command();
-const argv = program.opts(); 
-// const rl = readline.createInterface({
-//   input: process.stdin, // введення зі стандартного потоку
-//   output: process.stdout, // виведення у стандартний потік
-// });
 
 program
   .option('-a, --action <type>', 'choose action')
@@ -19,8 +9,7 @@ program
   .option('-p, --phone <type>', 'user phone');
 
 program.parse(process.argv);
-
-
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
 	switch (action) {
@@ -31,21 +20,22 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 			const getContact = await contacts.getContactById(id);
 			return console.log(getContact);
 		case 'add':
-			const addContact = await contacts.addContact(name, email, phone);
+			const addContact = await contacts.addContact({ name, email, phone });
 			return console.log(addContact);
-		case 'update':
-			const updateContact = await contacts.updateContact(id, { name, email, phone });
-			return console.log(updateContact);
 		case 'remove':
 			const deletedContact = await contacts.removeContact(id);
-			return console.log(deletedContact);
+			return console.log(deletedContact);	
+		case 'update': // additionally
+			const updateContact = await contacts.updateContact(id, { name, email, phone });
+			return console.log(updateContact);
     default:
-      console.warn('\x1B[31m Unknown action type!');
+      return console.warn('\x1B[31m Unknown action type!');
   }
 }
 
+invokeAction(argv);
 // invokeAction({ action: 'list' });
 // invokeAction({ action: 'get', id: 'e6ywwRe4jcqxXfCZOj_1e' });
-// invokeAction({ action: 'add', name: 'qwerty', email: '123@123', phone: '123-123-123'});
-// invokeAction({ action: 'remove', id:'e6ywwRe4jcqxXfCZOj_1e' });
-// invokeAction({ action: 'update', id: 'e6ywwRe4jcqxXfCZOj_1e', name: 'tyuiot', email: '5558@85', phone: '81-8-8' });
+// invokeAction({ action: 'add', name: 'New Contact', email: 'new123@gmail.com', phone: '123-123-123'});
+// invokeAction({ action: 'remove', id:'Z5sbDlS7pCzNsnAHLtDJd' });
+// invokeAction({ action: 'update', id: 'e6ywwRe4jcqxXfCZOj_1e', name: 'Update', email: 'test@mail.com', phone: '555-55-55' });
